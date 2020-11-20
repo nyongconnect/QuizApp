a0ef559c9e039039f0b9065cfad7fe722e1ebfe5
Nyong Nsikak <nyongconnect@gmail.com>
2020-11-20 07:50:21 +0100

    first commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+import java.util.*
+import javax.sql.DataSource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ local/src/main/java/com/example/local/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.local
+
+class MyClass {
+}
